9dd4c4b5ca798c8e186a3eb1e3d75a5d920d6de9
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2024-07-22 22:58:40 -0300

    01. Preparando o projeto

--- src/components/Avaliacao/index.tsx
+++ src/components/Avaliacao/index.tsx
@@ -14,11 +14,18 @@ function Avaliacao({ profissionais }: { profissionais: IProfissional[] | null })
     return (
         <>
             <SecaoCard>
-                {profissionais && profissionais?.length ? profissionais?.map((profissional) => {
-                    return <Card profissional={profissional} />
-                }) : 'Não há avaliações para mostrar'}
+                {profissionais && profissionais.length 
+                ? 
+                profissionais?.map((profissional) => {
+                  return <Card profissional={profissional} />
+                } )
+                : 
+                "Não há avaliações para mostrar"}
             </SecaoCard>
+{profissionais && profissionais.length > 4 ?
             <Botao>Ver mais</Botao>
+        : ""
+        }
         </>
     )
 }
